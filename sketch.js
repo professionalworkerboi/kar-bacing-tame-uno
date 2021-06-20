1ab004a02a6ca3af83269f786f602bfdf13762cd
professionalworkerboi <77230411+professionalworkerboi@users.noreply.github.com>
2021-06-20 17:43:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas,backgroundImg;
+var gameState = 0;
+var playerCount,database;
+var form,player,game;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+ function draw(){
+
+ }
